ce4ddf6faa653f5e716312a2853031beb8ac98d7
onnga-wasabi <yuuto.onnga.23@gmail.com>
2019-02-25 23:55:07 +0900

    __init__.py

---
+++ chaitorch/training/__init__.py
@@ -0,0 +1,4 @@
+from chaitorch.training import extension
+from chaitorch.training import trainer
+from chaitorch.training import trigger
+from chaitorch.training import updater
---
+++ chaitorch/utils/__init__.py
@@ -0,0 +1,1 @@
+from chaitorch.utils import reporter
