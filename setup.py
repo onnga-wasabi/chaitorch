f74d27dc6e11fe53bac1b58a4e59fc22b9b4ed9f
onnga-wasabi <yuuto.onnga.23@gmail.com>
2019-02-25 23:44:22 +0900

    add setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="chaitorch",
     version="0.1",
     description="pytroch wrapper dll framework likely chainer",
-    url="https://github.com/onnga-wasabi/chaitorch",
+    url="https://github.com/onnga-wasabi/pytorch-tutorial.git",
     packages=find_packages(),
 )
