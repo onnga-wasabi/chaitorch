c3f8a70c2b23fe95838be517d5306d842442014a
onnga-wasabi <yuuto.onnga.23@gmail.com>
2019-04-03 00:19:48 +0900

    update comment

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="chaitorch",
     version="0.1",
-    description="pytroch wrapper dll framework likely chainer",
+    description="Pytroch wrapper for those who want to coding w/o 'global'",
     url="https://github.com/onnga-wasabi/pytorch-tutorial.git",
     packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision',
+    ]
 )
