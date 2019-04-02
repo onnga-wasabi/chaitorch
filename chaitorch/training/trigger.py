23bda503ad48b966ee8f40b425667e2beb427161
onnga-wasabi <yuuto.onnga.23@gmail.com>
2019-04-03 01:18:41 +0900

    fixed bug

--- chaitorch/training/trigger.py
+++ chaitorch/training/trigger.py
@@ -11,7 +11,7 @@ class isTrigger(object):
             return (trainer.total_iter > 0) & (trainer.total_iter % (len(trainer.updater.data_loader) * self.trigger) == 0)
 
         elif self.target == 'iteration':
-            return (trainer.total_iter % self.trigger == 0)
+            return trainer.total_iter != 0 & (trainer.total_iter % self.trigger == 0)
 
 
 class BestValueTrigger(object):
